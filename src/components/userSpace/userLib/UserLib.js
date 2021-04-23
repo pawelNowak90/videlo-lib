8a2fd17357760e18a5b1a8b99dbadfee1e700fe9
pawel <pawel.nowak.90@gmail.com>
2021-04-23 14:25:58 +0200

    2021.04.23-1st-AddRandomAPI

--- src/components/userSpace/userLib/UserLib.js
+++ src/components/userSpace/userLib/UserLib.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 
 import DefaultCard from './DefaultCard';
 
@@ -6,17 +6,39 @@ import {Row} from 'reactstrap';
 
 let howManyColumns = "4";
 
-const UserLib = () => {
-    return (
+class UserLib extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { movies:[] }
+    }
+
+    URL = 'https://randomuser.me/api/?results=6';
+
+    fetchDataFromYTDataApi = () =>{
+        fetch(this.URL)
+            .then(resp => resp.json())
+            .then(data => this.setState({movies: data.results}))
+            .catch(error => console.log('F-a-i-l-u-r-e'));
+    }
+
+    componentDidMount(){
+        this.fetchDataFromYTDataApi();
+    }
+
+    render() {
+        const {movies} = this.state;
+        return (
         <Fragment>
             <h5>UserLib - komponent</h5>
+            <h5>{movies.length && "Tablica filmów została pobrana"}</h5>
             <Row>
                 <DefaultCard howManyColumns={howManyColumns}/>
                 <DefaultCard />
                 <DefaultCard />
             </Row>
-        </Fragment>
-     );
+            </Fragment>
+        );
+    }
 }
 
 export default UserLib;
